f39ac65117968db0c082c0c2eb14817a0d4b254a
Realmeir <meyirzhan.anarbek@gmail.com>
2022-11-29 17:19:00 +0600

    first commit

---
+++ src/Questions.java
@@ -0,0 +1,11 @@
+public class Questions {
+    public static void main(String[] args) {
+        System.out.println("Name: Meirzhan");
+        System.out.println("Surname: Anarbekuly");
+        System.out.println("Id: 220103307");
+        System.out.println("Hobby: Football");
+        System.out.println("How much do you like ICT(from 1-10)): 11");
+        System.out.println("Rate this task from 1 to 10: 11");
+        System.out.println("Am I the best teacher?: Of course, you are!");
+    }
+}
